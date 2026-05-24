173b56da1430aac8bec42bf83a0f384aba8f5479
Ikigai837 <luckysingh80307@gmail.com>
2026-05-24 21:25:33 +0530

    Update mac_unit.v

--- mac_unit.v
+++ mac_unit.v
@@ -3,17 +3,17 @@
 module mac_unit (
     input clk,
     input rst,
-    input signed [7:0] a,         // Matrix element (weight)
-    input signed [7:0] b,         // Vector element (activation)
-    output reg signed [15:0] prod // Synchronous product output
+    input signed [7:0] a,         
+    input signed [7:0] b,         
+    output reg signed [15:0] product
 );
 
-    // Stage 1 pipeline register
+
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            prod <= 16'sd0;
+            product <= 16'sd0;
         end else begin
-            prod <= a * b; 
+            product <= a * b; 
         end
     end
 
